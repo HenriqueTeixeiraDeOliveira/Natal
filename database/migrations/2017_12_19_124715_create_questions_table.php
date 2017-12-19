680db7cd03b265de845faef9099b098c84f67c46
Henrique Teixeira <henriquetcro@gmail.com>
2017-12-19 11:01:32 -0200

    Migrations created

---
+++ database/migrations/2017_12_19_124715_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question');
+            $table->string('first');
+            $table->string('second');
+            $table->string('third');
+            $table->string('fourth');
+            $table->string('fifth');
+            $table->unsignedTinyInteger('correct');
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
